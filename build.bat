9e8f7afe21292e765d1b4e0895875a63b8f1992d
Serkan Taş <serkan.tas@likyateknoloji.com>
2018-10-20 11:15:31 +0300

    .

--- build.bat
+++ build.bat
@@ -15,6 +15,10 @@ set MX_JS=%ROYALE_PATH%\royale-asjs\frameworks\projects\MXRoyale\target\MXRoyale
 set SPARK_SWC=%ROYALE_PATH%\royale-asjs\frameworks\projects\SparkRoyale\target\SparkRoyale-0.9.5-SNAPSHOT-swf.swc
 set SPARK_JS=%ROYALE_PATH%\royale-asjs\frameworks\projects\SparkRoyale\target\SparkRoyale-0.9.5-SNAPSHOT-js.swc
 
+set SPARK_MF=%ROYALE_PATH%\royale-asjs\frameworks\projects\SparkRoyale\src\main\resources\spark-royale-manifest.xml
+set MX_MF=%ROYALE_PATH%\royale-asjs\frameworks\projects\MXRoyale\src\main\resources\mx-royale-manifest.xml
+
+rem library://ns.apache.org/royale/mx %MX_MF%
 @echo on
 
-%COMPILER_PATH%\mxmlc %MAINCLASS% -external-library-path+=%MX_SWC%,%SPARK_SWC% -js-external-library-path+=%MX_JS%,%SPARK_JS% -source-path=%SRCPATH% -locale+=tr_TR
+%COMPILER_PATH%\mxmlc %MAINCLASS% -compiler.namespaces.namespace library://ns.apache.org/royale/spark %SPARK_MF% -external-library-path+=%MX_SWC%,%SPARK_SWC% -js-external-library-path+=%MX_JS%,%SPARK_JS% -source-path=%SRCPATH% -locale+=tr_TR
